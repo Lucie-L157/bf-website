6c7adb203bd31475db57d3be26b90ece1c0e65fe
Lucie <lucielazenby1@gmail.com>
2026-05-20 10:05:44 +0100

    Second  website upload

--- script.js
+++ script.js
@@ -22,5 +22,42 @@ $( document ).ready(function() {
         envelope.addClass("close")
            .removeClass("open");
     }
-   
+console.log("HELLO");
+    window.addEventListener('DOMContentLoaded', () => {
+
+    const root = document.querySelector('.heartsfloat');
+
+    const emojis = ['💖','💗','💞','💘','💕','💓'];
+
+    function addHeart() {
+
+        const s = document.createElement('span');
+
+        s.textContent =
+            emojis[Math.floor(Math.random() * emojis.length)];
+
+        s.style.left = Math.random() * 100 + '%';
+
+        s.style.fontSize =
+            (12 + Math.random() * 18) + 'px';
+
+        s.style.animationDuration =
+            (8 + Math.random() * 8) + 's';
+
+        root.appendChild(s);
+
+        setTimeout(() => {
+            s.remove();
+        }, 17000);
+    }
+
+    setInterval(addHeart, 600);
+
+    for(let i = 0; i < 10; i++) {
+        addHeart();
+    }
+
+});
+
+
 });
